a50478bbc890c49aa330f7d60458f389fa0882e9
Harry Dhillon <hdhillon478@gmail.com>
2023-12-02 20:26:36 +0000

    Add loading spinner , imports and profile image

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
+import Spinner from "../layout/Spinner";
 
 import GithubContext from "../../context/github/GithubContext";
 
 export default function User() {
-  const { user, fetchUser } = useContext(GithubContext);
+  const { user, fetchUser, loading } = useContext(GithubContext);
 
   const params = useParams();
 
@@ -12,5 +14,28 @@ export default function User() {
     fetchUser(params.login);
   }, []);
 
-  return <div>User</div>;
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <div className='w-full mx-auto lg:w-10/12'>
+        <div className='mb-4'>
+          <Link to='/' className='btn btn-ghost'>
+            Back To Search
+          </Link>
+        </div>
+        <div className='grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8'>
+          <div className='custom-card-image mb-6 md:mb-0'>
+            <div className='rounded-lg shadow-xl card image-full'>
+              <figure>
+                <img src={user.avatar_url} alt='Github Profile Avatar' />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
