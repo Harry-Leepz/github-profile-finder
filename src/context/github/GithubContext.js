e105427484bf14a428c3083760ad8f87ea292d74
Harry Dhillon <hdhillon478@gmail.com>
2023-12-04 12:44:11 +0000

    Remove console.log statement

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -42,7 +42,6 @@ export const GithubProvider = ({ children }) => {
       window.location = "/notfound";
     } else {
       const data = await response.json();
-      console.log(data);
       dispatch({
         type: "GET_USER",
         payload: data,
